1f1971d3535f2b85d88172af81921ebedfeb4a84
Eli Davidson <eli@shadowlab.dev>
2022-02-02 18:34:30 -0700

    Complete book file

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+
+  return mainCharacter;
+}
+
+function saveReview(review, reviews) {
+  if(!reviews.includes(review)) {
+    reviews.push(review);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  var pageCount = bookTitle.length * 20;
+  return pageCount; 
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(bookTitle)
+  };
+
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
