d17a1a4ff79ba3b9a75de8fd3209b0c41ac36ad9
Eli Davidson <eli@shadowlab.dev>
2022-02-04 13:04:49 -0700

    Complete librarian file

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,37 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
   }
+  greetPatron(patronName, isMorning) {
+    if(isMorning) {
+      return `Good morning, ${patronName}!`;
+    }
+    return `Hello, ${patronName}!`;
+  }
+  findBook(bookTitle) {
+    var availableGenres = [
+      'nonFiction',
+      'fiction',
+      'fantasy',
+    ]
+  
+    for (var i = 0; i < availableGenres.length; i++) {
+      var currentGenre = availableGenres[i];
+      for (var j = 0; j < this.library.shelves[currentGenre].length; j++) {
+        if (this.library.shelves[currentGenre][j].title === bookTitle) {
+          this.library.shelves[currentGenre].splice(j, 1);
+          return `Yes, we have ${bookTitle}`
+        }
+      }
+    } return `Sorry, we do not have ${bookTitle}`
+  }
+  
+  calculateLateFee(daysLate) {
+    var fee;
+    fee = Math.ceil(daysLate * .25);
+    return fee;
+  }  
 }
 
 module.exports = Librarian;
